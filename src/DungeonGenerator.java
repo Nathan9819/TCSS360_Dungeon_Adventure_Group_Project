8eb86eb0e0d8f6a59091bc4c79c7b0de31249695
Nathan Mahnke <nmahnke@uw.edu>
2022-07-16 22:36:16 -0700

    Reworked dungeon generation. Rooms will be generated as nodes with a random number of exits between 0 and 3, these exits will be checked for validity and a room will be generated connected to the exits if the max room count has not been reached.

--- src/DungeonGenerator.java
+++ src/DungeonGenerator.java
@@ -1,73 +1,92 @@
+import java.awt.Dimension;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class DungeonGenerator {
-    public static room[][] maze = new room[21][21];
+    public static room[][] dungeon = new room[21][21];
+    public static ArrayList<Point> prospects;
     private final int MAX_ROOM_COUNT = 40;
     private int roomCount = 0;
 
     public DungeonGenerator() {
         fillMaze();
-        generateRoom(20, 10, 0);
-        generateMaze(19, 10, 0);
+        startGame();
         displayMaze();
     }
 
     public void fillMaze() {
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze[0].length; j++) {
-                maze[i][j] = new room(0);
+        for (int i = 0; i < dungeon.length; i++) {
+            for (int j = 0; j < dungeon[0].length; j++) {
+                dungeon[i][j] = new room(" ", new Point(i, j));
             }
         }
     }
 
-    public void generateMaze(int row, int col, int lastDirection) {
-        if (roomCount < MAX_ROOM_COUNT) {
-            boolean valid = false;
-            int nextDirection = -1;
-            while (!valid) {
-                Random rand = new Random();
-                int nextStep = rand.nextInt(6);
-                if (nextStep > 3) {
-                    nextDirection = lastDirection;
-                } else {
-                    nextDirection = (lastDirection + nextStep) % 4;
-                }
-                valid = testMove(row, col, nextDirection);
-                if (Math.abs(nextDirection - lastDirection) == 2) {
-                    valid = false;
-                }
-            }
-            generateRoom(row, col, nextDirection);
+    public void startGame() {
+        dungeon[dungeon[0].length / 2][0].roomContents = "S";
+        dungeon[dungeon[0].length / 2][0].east = dungeon[dungeon[0].length / 2][1];
+        dungeon[dungeon[0].length / 2][0].coords = new Point(dungeon[0].length / 2, 0);
+        dungeon[dungeon[0].length / 2][1].roomContents = "P";
+        dungeon[dungeon[0].length / 2][1].west = dungeon[dungeon[0].length / 2][0];
+        dungeon[dungeon[0].length / 2][1].coords = new Point(dungeon[0].length / 2, 1);
 
-            roomCount++;
+        while (hasProspects() && roomCount < MAX_ROOM_COUNT) {
+            generateRoom();
         }
     }
 
-    public static boolean testMove(int row, int col, int direction) {
-        if (direction == 0) {
-            return row - 1 >= 0;
-        } else if (direction == 1) {
-            return col + 1 < 21;
-        } else if (direction == 2) {
-            return row + 1 < 21;
-        } else if (direction == 3) {
-            return col - 1 >= 0;
+    public boolean hasProspects() {
+        for (int row = 0; row < dungeon.length; row++) {
+            for (int col = 0; col < dungeon[0].length; col++) {
+                if (dungeon[row][col].roomContents.equals("P")) {
+                    return true;
+                }
+            }
         }
         return false;
     }
 
-    public void generateRoom(int x, int y, int direction) {
-//        Random rand = new Random();
-//        int contents = rand.nextInt(3) + 1;
-        maze[x][y].roomContents = 1;
-        if (x + 1 < 21) {
-            maze[x + 1][y].west = true;
+    public void generateRoom() {
+        getProspects().stream().forEach((p) -> {
+
+        });
+    }
+
+    public boolean validMove(room room, int direction) {
+        // If the move is north
+        if (direction == 0 && room.coords.x - 1 >= 0) {
+            return dungeon[room.coords.x - 1][room.coords.y].roomContents.equals(" ");
+        // If move is east
+        } else if (direction == 1 && room.coords.y + 1 == dungeon[0].length) {
+            return dungeon[room.coords.x][room.coords.y + 1].roomContents.equals(" ");
+        // If move is south
+        } else if (direction == 2 && room.coords.x + 1 == dungeon.length) {
+            return dungeon[room.coords.x + 1][room.coords.y].roomContents.equals(" ");
+        // If move is west
+        } else if (direction == 3 && room.coords.y - 1 >= 0) {
+            return dungeon[room.coords.x + 1][room.coords.y].roomContents.equals(" ");
+        } else {
+            return false;
         }
+    }
+
+    public List<Point> getProspects() {
+        ArrayList points = new ArrayList();
+        for (int row = 0; row < dungeon.length; row++) {
+            for (int col = 0; col < dungeon[0].length; col++) {
+                if (dungeon[row][col].roomContents.equals("P")) {
+                    points.add(new Point(row, col));
+                }
+            }
+        }
+        return points;
     }
 
     public static void displayMaze() {
         System.out.print("\t ");
-        for(int i = 0; i < maze.length; i++) {
+        for(int i = 0; i < dungeon.length; i++) {
             if (i < 10) {
                 System.out.print(" " + i + "  ");
             } else {
@@ -75,10 +94,10 @@ public class DungeonGenerator {
             }
         }
         System.out.print("\n");
-        for (int i = 0; i < maze.length; i++) {
+        for (int i = 0; i < dungeon.length; i++) {
             System.out.print(i + "\t| ");
-            for (int j = 0; j < maze[0].length; j++) {
-                System.out.print(maze[i][j].roomContents);
+            for (int j = 0; j < dungeon[0].length; j++) {
+                System.out.print(dungeon[i][j].roomContents);
                 System.out.print(" | ");
             }
             System.out.print("\n");
@@ -86,11 +105,13 @@ public class DungeonGenerator {
     }
 
     private class room {
-        public int roomContents;
-        public boolean north, east, south, west = false;
+        public String roomContents;
+        public room north, east, south, west = null;
+        public Point coords;
 
-        private room(int contents) {
+        private room(String contents, Point xy) {
             roomContents = contents;
+            coords = xy;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.awt.*;
+
 public class Main {
-// This is a test file which has been added to GitHub via Intellij IDEA
+
     public static void main(String[] args) {
-	// write your code here
+        DungeonGenerator d = new DungeonGenerator();
     }
+
 }
