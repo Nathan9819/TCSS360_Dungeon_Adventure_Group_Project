1061fbe07cc9b6ace537d5805a5ee57a79f300b3
Nathan Mahnke <nmahnke@uw.edu>
2022-07-18 18:21:54 -0700

    Huge improvements made to dungeon generation. Current bug: Rooms can generate, then choose to generate a room in the position of the room that generated it on rare occasion.

--- src/DungeonGenerator.java
+++ src/DungeonGenerator.java
@@ -1,4 +1,3 @@
-import java.awt.Dimension;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,13 +6,13 @@ import java.util.Random;
 public class DungeonGenerator {
     public static room[][] dungeon = new room[21][21];
     public static ArrayList<Point> prospects;
-    private final int MAX_ROOM_COUNT = 40;
-    private int roomCount = 0;
+    private final int MAX_EXPANSION_COUNT = 6;
+    private int count = 0;
 
     public DungeonGenerator() {
         fillMaze();
         startGame();
-        displayMaze();
+        displayDungeon();
     }
 
     public void fillMaze() {
@@ -31,9 +30,15 @@ public class DungeonGenerator {
         dungeon[dungeon[0].length / 2][1].roomContents = "P";
         dungeon[dungeon[0].length / 2][1].west = dungeon[dungeon[0].length / 2][0];
         dungeon[dungeon[0].length / 2][1].coords = new Point(dungeon[0].length / 2, 1);
+//        dungeon[0][0].roomContents = "S";
+//        dungeon[0][0].east = dungeon[0][1];
+//        dungeon[0][0].coords = new Point(0, 0);
+//        dungeon[0][1].roomContents = "P";
+//        dungeon[0][1].west = dungeon[0][0];
+//        dungeon[0][1].coords = new Point(0, 1);
 
-        while (hasProspects() && roomCount < MAX_ROOM_COUNT) {
-            generateRoom();
+        while (hasProspects() && count < MAX_EXPANSION_COUNT) {
+            generateRooms();
         }
     }
 
@@ -48,10 +53,55 @@ public class DungeonGenerator {
         return false;
     }
 
-    public void generateRoom() {
-        getProspects().stream().forEach((p) -> {
+    public void generateRooms() {
+        count++;
+        for (Point p : getProspects()) {
+            displayDungeon();
+            Random rand = new Random();
+            int numOfExits;
+            numOfExits = rand.nextInt(2) + 1;
+            dungeon[p.x][p.y].roomContents = "S";
+            int randDirection;
+            randDirection = rand.nextInt(3);
+            while (numOfExits > 0) {
+                if (validMove(dungeon[p.x][p.y], randDirection)) {
+                    createRoom(p, randDirection);
+                }
+                randDirection++;
+                if (randDirection > 3) {
+                    randDirection = 0;
+                }
+                numOfExits--;
+            }
+        }
+    }
 
-        });
+    public void createRoom(Point p, int direction) {
+        if (direction == 0) {
+            dungeon[p.x][p.y].roomContents = "S";
+            dungeon[p.x][p.y].north = dungeon[p.x - 1][p.y];
+            dungeon[p.x - 1][p.y].roomContents = "P";
+            dungeon[p.x - 1][p.y].coords = new Point(p.x, p.y);
+            dungeon[p.x - 1][p.y].south = dungeon[p.x][p.y];
+        } else if (direction == 1) {
+            dungeon[p.x][p.y].roomContents = "S";
+            dungeon[p.x][p.y].east = dungeon[p.x][p.y + 1];
+            dungeon[p.x][p.y + 1].roomContents = "P";
+            dungeon[p.x][p.y + 1].coords = new Point(p.x, p.y + 1);
+            dungeon[p.x][p.y + 1].west = dungeon[p.x][p.y];
+        } else if (direction == 2) {
+            dungeon[p.x][p.y].roomContents = "S";
+            dungeon[p.x][p.y].north = dungeon[p.x + 1][p.y];
+            dungeon[p.x + 1][p.y].roomContents = "P";
+            dungeon[p.x + 1][p.y].coords = new Point(p.x, p.y);
+            dungeon[p.x + 1][p.y].south = dungeon[p.x][p.y];
+        } else if (direction == 3) {
+            dungeon[p.x][p.y].roomContents = "S";
+            dungeon[p.x][p.y].east = dungeon[p.x][p.y - 1];
+            dungeon[p.x][p.y - 1].roomContents = "P";
+            dungeon[p.x][p.y - 1].coords = new Point(p.x, p.y - 1);
+            dungeon[p.x][p.y - 1].west = dungeon[p.x][p.y];
+        }
     }
 
     public boolean validMove(room room, int direction) {
@@ -59,14 +109,15 @@ public class DungeonGenerator {
         if (direction == 0 && room.coords.x - 1 >= 0) {
             return dungeon[room.coords.x - 1][room.coords.y].roomContents.equals(" ");
         // If move is east
-        } else if (direction == 1 && room.coords.y + 1 == dungeon[0].length) {
+        } else if (direction == 1 && room.coords.y + 1 < dungeon[0].length) {
             return dungeon[room.coords.x][room.coords.y + 1].roomContents.equals(" ");
         // If move is south
-        } else if (direction == 2 && room.coords.x + 1 == dungeon.length) {
+        } else if (direction == 2 && room.coords.x + 1 < dungeon.length) {
             return dungeon[room.coords.x + 1][room.coords.y].roomContents.equals(" ");
         // If move is west
         } else if (direction == 3 && room.coords.y - 1 >= 0) {
             return dungeon[room.coords.x + 1][room.coords.y].roomContents.equals(" ");
+        // Room was on edge of board and thus could not move off it
         } else {
             return false;
         }
@@ -84,7 +135,7 @@ public class DungeonGenerator {
         return points;
     }
 
-    public static void displayMaze() {
+    public static void displayDungeon() {
         System.out.print("\t ");
         for(int i = 0; i < dungeon.length; i++) {
             if (i < 10) {
