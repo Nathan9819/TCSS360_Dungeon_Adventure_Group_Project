c3cb64ed3ea7f8557bf38935edf7a19545d1dfd4
alay22405 <77366927+alay22405@users.noreply.github.com>
2022-07-31 15:08:08 -0700

    HealingPotion

--- HealingPotion.java
+++ HealingPotion.java
@@ -1,24 +1,37 @@
-package main;
-import java.util.Random;
+//extend HealingPotion class with the pickupItem
 public class HealingPotion extends PickupItem {
-    public int HealingPotion;
-    public int minHealingPotion;
-    public int maxHealingPotion;
-    String player = "HealingPotion";
+    // protected static int HealingPotion;
+    //declared
+    protected static int minHealingPotion;
+    protected static int maxHealingPotion;
+    private static char HealingPotionSymbol;
+    private static String potionName;
+    private static String potionDescription;
+    //private char symbol;
+//constructor to intialize the class
+    public HealingPotion(){
+        //intilialize the parent class by 'super'
+        super(potionDescription, potionDescription, HealingPotionSymbol);
+        //this.HealingPotion=0;
+        //intialize
+        this.minHealingPotion=5;
+        this.maxHealingPotion=15;
+        this.HealingPotionSymbol='H';
+        this.potionName="HealingPotion";
+        this.potionDescription="A potion is healed from 5 to 15 HP";
+    }
+    //provide access to the healing symbol
+    public static  char getHealingSymbol() {
+        return HealingPotionSymbol;
+    }
+    //provide access to potion name
+    public String getPotionName(){
 
-    public HealingPotion() {
-        this(null);
-        this.HealingPotion=0;
-        this.minHealingPotion=3;
-        this.maxHealingPotion=9;
+        return potionName;
     }
-    public void work (DungeonCharacter dungencharacter) {
-        //System.out.println(" ");
-        //System.out.println(" ");
+    //provide access to the description
+    public String getDescription(){
+
+        return potionDescription;
     }
-    public HealingPotion(DungeonGenerator dungeon) {
-        super("HealingPotion", dungeon);
-        Random getRandomHealing = new Random();
-        this.HealingPotion = minHealingPotion + getRandomHealing.nextInt(maxHealingPotion);
     }
-}
