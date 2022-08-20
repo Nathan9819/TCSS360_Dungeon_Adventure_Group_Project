607b44097a3baace9672bbc3a6bd853fa676755e
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 14:31:09 -0700

    Update Hero.java

--- src/battles/Hero.java
+++ src/battles/Hero.java
@@ -1,9 +1,25 @@
 import java.util.Random;
 
+/**
+ * The Hero class. Extends DungeonCharacter.
+ * Is abstract, and requires its child classes to define some methods.
+ * 
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public abstract class Hero extends DungeonCharacter {
+    /**
+     *The Block chance.
+     */
     private double myBlock;
+    /**
+     * A Random variable.
+     */
     private Random rand = new Random();
 
+    /**
+     * The default constructor. Shouldn't be activated, but is here for safety.
+     */
     protected Hero() {
         super();
         setBlock(0.2);
@@ -23,11 +39,27 @@ public abstract class Hero extends DungeonCharacter {
         super(theMaxHP, theMinDMG, theMaxDMG, theName, theAtkSpd, theHit);
         setBlock(theBlock);
     }
-
+    /**
+     * Sets the block value.
+     * Does not allow values below 0 or above 1.
+     * 
+     * @param theBlock
+     */
     protected void setBlock(final double theBlock) {
+        if (theBlock < 0 || theBlock > 1) {
+            System.out.println("Block chance must be within the range of 0-1.");
+            return;
+        }
         myBlock = theBlock;
     }
 
+    /**
+     * Overrides the takeDMG method from DungeonCharacter.
+     * Checks if the hero should block damage before taking it.
+     * 
+     * @param theDMG
+     * @return A String that explains what happened
+     */
     @Override
     public String takeDMG(final int theDMG) {
         StringBuilder dmgInfo = new StringBuilder();
@@ -37,5 +69,12 @@ public abstract class Hero extends DungeonCharacter {
         }
         return super.takeDMG(theDMG);
     }
+    /**
+     * An abstract method. Child classes must define it.
+     * Will determine a class's special attack.
+     * 
+     * @param theOther
+     * @return A String that describes what happened
+     */
     public abstract String special(final Monster theOther);
 }
