94314df0b183fc66e082daa7b56813416463929e
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 14:11:02 -0700

    Update Automaton.java

--- src/battles/Automaton.java
+++ src/battles/Automaton.java
@@ -1,8 +1,23 @@
+/**
+ * The Automaton class. Extends Monster.
+ * Cannot be a form of Monster as it has unique mechanics.
+ * 
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public class Automaton extends Monster {
     public Automaton() {
-//        super(125, 25, 40, "Automaton", 3,  0.6, 0.0, 0, 0);
+        super(125, 25, 40, "Automaton", 3,  0.6, 0.0, 0, 0);
         addShield(10);
     }
+    /**
+     * Overrides the standard TakeDMG method.
+     * Will restore 10 shield after taking any damage.
+     * 
+     * @param theDMG
+     * @return a String that explains what happened
+     */
+    @Override
     public String takeDMG(final int theDMG) {
         String myString = super.takeDMG(theDMG);
         if (getHP() > 0) {
