d4f52bb1551b3263943cd803bd8705b43ec5bb88
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 15:15:00 -0700

    Update Ogre.java

--- src/battles/Ogre.java
+++ src/battles/Ogre.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
 
+/**
+ * The Ogre class. Extends Mosnter.
+ *
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public class Ogre extends Monster {
     public Ogre() {
             super(200, 30, 60, "Ogre", 2, 0.6, 0.1, 30, 60, 27, 52,
             new ImageIcon(Ogre.class.getResource("DungeonGeneration/Assets/Ogre.png")), 200, -16, 12);
-}
+    }
 }
