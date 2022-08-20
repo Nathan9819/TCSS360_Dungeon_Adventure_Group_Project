6bfcfbf907acc1b9ba27b454926f8279dc922032
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 15:37:47 -0700

    Update Warrior.java

--- src/battles/Warrior.java
+++ src/battles/Warrior.java
@@ -1,10 +1,22 @@
 import java.util.Random;
 
+/**
+ * The Warrior class. Extends Hero.
+ * 
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public class Warrior extends Hero {
     public Warrior() {
         super(125, 35, 60, "Warrior", 4, 0.8, 0.2);
 //        super(125, 1000, 1001, "Warrior", 4, 1, 0.2);
     }
+    /**
+     * The Special attack for Warrior. Allows it to do massive damage, but with low accuracy.
+     * 
+     * @author Colton Wickens
+     * @version August 6, 2022
+     */
     public String special(final Monster theOther) {
         StringBuilder myAtkInfo = new StringBuilder();
         myAtkInfo.append(super.getName() + " performed Crushing Blow!\n");
