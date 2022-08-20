c47a58db5698883c249cbb0402e1df42b7d280e2
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 14:17:23 -0700

    Update Gremlin.java

--- src/battles/Gremlin.java
+++ src/battles/Gremlin.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
 
+/**
+ * The Gremlin class. Extends Monster.
+ *
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public class Gremlin extends Monster {
     public Gremlin() {
         super(70, 15, 30, "Gremlin", 5, 0.8, 0.4, 20, 40, 40, 42,
