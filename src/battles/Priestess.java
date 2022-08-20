336c0e74fa16d0d9893f9c2184aef80aa667869c
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 15:23:07 -0700

    Update Priestess.java

--- src/battles/Priestess.java
+++ src/battles/Priestess.java
@@ -1,9 +1,23 @@
 import java.util.Random;
 
+/**
+ * The Priestess class. Exends Hero.
+ * 
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public class Priestess extends Hero {
+    /**
+     * Priestess constructor.
+     */
     public Priestess() {
         super(75, 25, 45, "Sorceress", 5, 0.7, 0.3);
     }
+    /**
+     * Priestess's special attack. Grants herself a shield.
+     * 
+     * @param theOther
+     */
     public String special(final Monster theOther) {
         StringBuilder myAtkInfo = new StringBuilder();
         myAtkInfo.append(super.getName() + " used Divine Shield!");
