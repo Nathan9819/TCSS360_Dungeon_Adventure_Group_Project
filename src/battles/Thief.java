9e08d7a105cbe950d806a113caf11f1ac97d3a4f
DraconicTyrant <80440145+DraconicTyrant@users.noreply.github.com>
2022-08-20 15:32:36 -0700

    Update Thief.java

--- src/battles/Thief.java
+++ src/battles/Thief.java
@@ -1,9 +1,21 @@
 import java.util.Random;
 
+/**
+ * The Thief Class. Extends Hero.
+ * 
+ * @author Colton Wickens
+ * @version August 6, 2022
+ */
 public class Thief extends Hero {
     public Thief() {
         super(75, 20, 40, "Thief", 6, 0.8, 0.2);
     }
+    /**
+     * Thief's special attack. Allows the Thief to attack twice if the odds are in his favor.
+     * 
+     * @param theOther
+     * @return a String describing what happened here
+     */
     public String special(final Monster theOther) {
         StringBuilder myAtkInfo = new StringBuilder();
         Random rand = new Random();
